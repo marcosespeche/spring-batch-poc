4672518c291e398fc464011c1ff03fdbe1d74d48
marcosespeche <marcos.espeche.28@gmail.com>
2025-12-23 14:59:29 -0300

    Creating project base structure

---
+++ src/main/java/com/marcosespeche/spring_batch_poc/SpringBatchPocApplication.java
@@ -0,0 +1,17 @@
+package com.marcosespeche.spring_batch_poc;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class SpringBatchPocApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = SpringApplication.run(SpringBatchPocApplication.class, args);
+
+		String port = context.getEnvironment().getProperty("server.port", "8080");
+		System.out.println("Running application on port " + port);
+	}
+}
---
+++ src/test/java/com/marcosespeche/spring_batch_poc/SpringBatchPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosespeche.spring_batch_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
